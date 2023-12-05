570504406505c9ca2e18210745245b5d7e3725ec
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-12-05 02:55:41 -0800

    Task4:Create BaseModel From Dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -9,29 +9,45 @@ class BaseModel():
     Defines all common attributes/methods for other classes.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialize object attributes.
         Attributes:
             id (string): assign with an uuid when an instance is created.
-            created_at (datetime): the current datetime when an instance is created.
-            updated_at (datetime): assign with the current datetime when an instance
-                        is created and it will be updated every time you change your object.
+            created_at (datetime): the current datetime when an instance
+                        is created.
+            updated_at (datetime): assign with the current datetime
+                        when an instance is created and it will be
+                        updated every time you change your object.
         """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for k, v in kwargs.items():
+                if k == "updated_at" or k == "created_at":
+                    self.__dict__[k] = datetime.fromisoformat(v)
+                elif k == "__class__":
+                    continue
+                elif k == "id":
+                    self.__dict__[k] = str(v)
+                else:
+                    self.__dict__[k] = v
 
     def __str__(self):
         """Print: [<class name>] (<self.id>) <self.__dict__>"""
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                self.id, self.__dict__))
 
     def save(self):
-        """Updates the public instance attribute updated_at with the current datetime"""
+        """Updates the public instance attribute updated_at
+        with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """Returns a dictionary containing all
+        keys/values of __dict__ of the instance"""
         obj_dict = {}
         obj_dict["__class__"] = self.__class__.__name__
         for k, v in self.__dict__.items():
