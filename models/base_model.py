0091fb93fae0a3033e8ec53d8482a34a03a905bf
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-12-05 05:27:35 -0800

    Task5. Store first object in file.json

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Defines the BaseModel Class """
 from uuid import uuid4
 from datetime import datetime
+import models
 
 
 class BaseModel():
@@ -21,9 +22,10 @@ class BaseModel():
                         updated every time you change your object.
         """
         if not kwargs:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
         else:
             for k, v in kwargs.items():
                 if k == "updated_at" or k == "created_at":
@@ -44,6 +46,7 @@ class BaseModel():
         """Updates the public instance attribute updated_at
         with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all
