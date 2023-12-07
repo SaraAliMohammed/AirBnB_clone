c1823f286af8b87c31c6c07b3430f77943ea8a8a
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-12-07 01:17:55 -0800

    Task8: Add class User

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 """Defines A FileStorage Class
-import json
-from models.base_modele import BaseModel
-from models.user import User
 from models.place import Place
 from models.amenity import Amenity
 from models.city import City
@@ -11,7 +8,7 @@ from models.state import State
 """
 import json
 from models.base_model import BaseModel
-
+from models.user import User
 
 class FileStorage():
     """
@@ -25,7 +22,7 @@ class FileStorage():
     """
     __file_path = "file.json"
     __objects = {}
-    classes_dict = {"BaseModel": BaseModel}
+    classes_dict = {"BaseModel": BaseModel, "User": User}
 
     def all(self):
         """Returns the dictionary __objects"""
